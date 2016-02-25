a6b531d4c0af1d16eff6d4c078e8b89c0f4c54c3
Simranjit <simranjit2701@gmail.com>
2016-02-25 20:58:10 +1100

    When iPhone is shared a random sound will be generated along with it , image will be displayed for the sound

--- ShakingSounds/ViewController.swift
+++ ShakingSounds/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var soundsImage: UIImageView!
+    var player:AVAudioPlayer = AVAudioPlayer()
+    
+    var sounds = ["radiotuning","splash","steamengine"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        
+        if event?.subtype == UIEventSubtype.MotionShake{
+            
+            let randomNumber = Int(arc4random_uniform(UInt32(sounds.count)))
+            
+            let fileLocation = NSBundle.mainBundle().pathForResource(sounds[randomNumber], ofType: "mp3")
+            
+            do{
+                try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: fileLocation!))
+                player.play()
+                soundsImage.image = UIImage(named:"\(sounds[randomNumber])"+".jpg")
+            }catch{
+                print("error")
+            }
+         
+        
+        }
     }
 
     override func didReceiveMemoryWarning() {
